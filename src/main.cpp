4fede533fbd7f164e33f77304230be3dd58e3a4c
Alphalaneous <38200084+Alphalaneous@users.noreply.github.com>
2024-09-14 21:53:38 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -51,12 +51,10 @@ class $modify(MyEditorUI, EditorUI) {
         if (exists) {
             m_fields->m_existingSounds[path] = true;
 
-            #ifndef GEODE_IS_ANDROID
             FMODAudioEngine::sharedEngine()->m_channelGroup2->setPaused(false);
             FMODAudioEngine::sharedEngine()->m_backgroundMusicChannel->setPaused(false);
             FMODAudioEngine::sharedEngine()->m_globalChannel->setPaused(false);
             FMODAudioEngine::sharedEngine()->m_system->update();
-            #endif
             FMODAudioEngine::sharedEngine()->playEffectAdvanced(path, 1, 0, 1, pitch, true, false, 0, 0, 0, 0, false, 0, false, true, 0, 0, 0, 0);
         }
     }
